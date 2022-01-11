f1abd153fe78ec45a6d4e6b6a557726e7d762a1a
rohan-innovaccer <rohan.mathur@innovaccer.com>
2022-01-11 19:04:37 +0530

    refactor for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,17 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles.css';
-<<<<<<< HEAD
-import Sidebar from './components/Sidebar';
-=======
 import Login from './pages/Login';
 
->>>>>>> 2192d8dbd6ce76d27dc57e0d39e5029428c894df
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-<<<<<<< HEAD
-        <nav className='navigation'>
-          <div className='logo'>
-            <span>in</span>record 🚀
-          </div>
-          <ul className='list'>
-            <li className='list-item'>About us</li>
-            <li className='list-item'>Login</li>
-          </ul>
-        </nav>
-        <div className='main-container'>
-          <Sidebar />
-          <div className='content-wrapper'>
-            <Routes>
-              <Route path='/' exact element={<Home />} />
-              <Route path='/register' element={<Register />} />
-            </Routes>
-          </div>
-        </div>
-=======
         <h1>This is navigation</h1>
         <Routes>
           <Route path='/home' exact element={<Home />} />
           <Route path='/login' element={<Login />} />
         </Routes>
->>>>>>> 2192d8dbd6ce76d27dc57e0d39e5029428c894df
       </div>
     </BrowserRouter>
   );
