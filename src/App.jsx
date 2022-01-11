44de58c733e3fa8fbdaf7fcdd7416df9ad61833f
rohan-innovaccer <rohan.mathur@innovaccer.com>
2022-01-11 16:50:02 +0530

    feat: add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
+import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles.css';
 
@@ -9,7 +10,8 @@ const App = () => {
       <div>
         <h1>This is navigation</h1>
         <Routes>
-          <Route path='/home' exact element={<Home />} />
+          <Route path='/' exact element={<Home />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </div>
     </BrowserRouter>
