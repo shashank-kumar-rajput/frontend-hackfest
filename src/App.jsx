3ee50e47c1a3e2d88bb23dd68c8948ad62181359
rohan-innovaccer <rohan.mathur@innovaccer.com>
2022-01-11 13:03:52 +0530

    feat: add home page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>This is navigation</h1>
+        <Routes>
+          <Route path='/home' exact element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
