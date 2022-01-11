48c0af175059cc722b5a471a82f709b7c8752749
hsk303 <kushwahharshit1998@gmail.com>
2022-01-11 16:34:52 +0530

    feat: add sidebar for components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles.css';
-
+import Sidebarcomp from './components/Sidebar/Sidebar';
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <h1>This is navigation</h1>
+        <Sidebarcomp/>
         <Routes>
           <Route path='/home' exact element={<Home />} />
         </Routes>
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+
+import React from 'react';
+// import { Navigation } from '@innovaccer/design-system';
+const Sidebarcomp=() => {
+   return (
+    <nav class="navbar bg-light">
+
+    <div class="container-fluid">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+    <a class="nav-link" href="#">Medical Summary</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Problem List</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Diagnostic Results</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Past History of Illness</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Plan of Care</a>
+    </li>
+    </ul>
+    </div>
+    </nav>
+   )
+   
+    
+  }
+export default Sidebarcomp;
+      
