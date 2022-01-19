30e3087e842fb1e4c06a0462a847f3e04da637c7
shashank-kumar-rajput <shashank4717@gmail.com>
2022-01-19 17:26:55 +0530

    test: testcase for login page

---
+++ src/first-sample-build.js
@@ -0,0 +1,13 @@
+
+const {Builder, By, Key, until} = require("selenium-webdriver");
+const username="Himanch"
+const password="Hello@123"
+async function example(){
+    let driver=await new Builder().forBrowser("chrome").build();
+    await driver.get("https://inrecord.netlify.app/");
+    await driver.findElement(By.xpath('//*[@id="root"]/div/nav/ul/li/a')).click();
+    await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/div/div/div/form/div[2]/input')).sendKeys(username,Key.RETURN);
+    await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/div/div/div/form/div[4]/input')).sendKeys(password,Key.RETURN);
+    await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/div/div/div/form/button')).click()
+}
+example()
