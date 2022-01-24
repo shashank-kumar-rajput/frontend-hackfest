64f8ca31a4cfc4e5e565ad26d2aa02598f9445b0
rohan-innovaccer <rohan.mathur@innovaccer.com>
2022-01-24 14:14:48 +0530

    test: resolve npm CI tests.yml

--- src/pages/Medication/Medication.test.js
+++ src/pages/Medication/Medication.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
 import Medication from './index';
 
 // mock setup for server
